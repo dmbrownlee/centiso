b1276e2cb5ccc1a1fd6f042aca287ef087bddaaa
dmbrownlee <dmbrownlee@users.noreply.github.com>
2017-07-16 21:24:46 -0700

    Initial commit of dnsmasq role.

---
+++ ansible/roles/dnsmasq/templates/dnsmasq.conf.js
@@ -0,0 +1,21 @@
+interface={{ iface_int }}
+domain={{ dnsdomain }}
+
+# DHCP Address Pool
+dhcp-range= {{ iface_int }},{{ dhcp_range_bottom }},{{ dhcp_range_top }},{{ dhcp_range_mask }},{{ dhcp_lease_duration }}
+
+# DHCP Options
+# 3   = gateway address
+# 6   = nameserver address
+# 28  = broadcast address
+# 42  = NTP server address
+dhcp-option=3,{{ gateway_address }}
+dhcp-option=6,{{ nameserver_address }},{{ nameserver2_address }}
+dhcp-option=28,{{ broadcast_address }}
+#dhcp-option=42,{{ ntpserver_address }}
+
+# PXE Configuration
+dhcp-boot=pxelinux.0,pxeserver,{{ pxeserver_address }}
+
+# Example Static IP address reservations
+# dhcp-host=50:e5:49:cb:c4:84,{{ dhcp_range_bottom }}
